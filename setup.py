8338e050d68a23237266d107133cfeca97bcacd0
Jacob Smith <enforcie@gmail.com>
2021-11-21 22:28:25 -0500

    Adding a setup.py script

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+        name='feed-builder',
+        version='1.0',
+        description='builds my atom feeds',
+        author='Billie Enforcie',
+        author_email='lefunnypickleman@protonmail.com',
+        packages=[],
+        install_requires=['bs4'],
+        py_modules=['feed-builder'],
+        )
